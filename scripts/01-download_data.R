e777e58c681cad32b0c9a93255885c550f916158
karenrni <clairriani@gmail.com>
2024-12-01 03:00:01 -0500

    download

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -11,21 +11,21 @@
 library(opendatatoronto)
 library(tidyverse)
 
-
 #### Download data ####
+
 # get package
-package <- show_package("e28bc818-43d5-43f7-b5d9-bdfb4eda5feb")
+package <- show_package("058236d2-d26e-4622-9665-941b9e7a5229")
+package
 
 # get all resources for this package
-resources <- list_package_resources("e28bc818-43d5-43f7-b5d9-bdfb4eda5feb")
+resources <- list_package_resources("058236d2-d26e-4622-9665-941b9e7a5229")
 
 # identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
 datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
 
 # load the first datastore resource as a sample
-data <- filter(datastore_resources, row_number()==1) %>% 
-  get_resource()
-
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+data
 
 #### Save data ####
 write_csv(data, "data/raw_data/raw_data.csv") 
