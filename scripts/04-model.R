be59b9cf81c9df39453b17751e18b1cb72d1b58d
karenrni <clairriani@gmail.com>
2024-12-03 06:51:46 -0500

    little updates

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,30 +1,20 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
+# Purpose:  Checking and Fitting Models and Making Predictions
 # Author: Karen Riani
-# Date: 1 December 2024
+# Date: 3 December 2024
 # Contact: karen.riani@mail.utoronto.ca
-# Pre-requisites: None
-# Any other information needed? None
-
+# Pre-requisites: Run 02-data_cleaning.R script
+# Any other information needed? None.
 
 #### Workspace setup ####
 library(tidyverse)
-library(rstanarm)
+library(arrow)
+library(testthat)
 
-#### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+#### Test data ####
 
-### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
+# Load the cleaned dataset
+final_filtered_data <- arrow::read_parquet("data/analysis_data/analysis_data.parquet")
 
 
 #### Save model ####
